179fe481e61b66f7d07116d9929c9df1cd4ebcfb
Paweł Prażak <pawel.prazak@gmail.com>
2021-01-19 19:08:29 +0100

    Bump version to v0.0.9
    Signed-off-by: Paweł Prażak <pawel.prazak@gmail.com>

--- internal/version/version.go
+++ internal/version/version.go
@@ -3,7 +3,7 @@ package version
 import "fmt"
 
 // VERSION indicates which version of the binary is running.
-var VERSION = "v0.0.8"
+var VERSION = "v0.0.9"
 
 // GITCOMMIT indicates which git hash the binary was built off of
 var GITCOMMIT = ""
